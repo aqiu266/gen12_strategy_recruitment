f37225ad342a431e7ed1b5c9e5ffc989dab644c5
aqiu266 <147431324+aqiu266@users.noreply.github.com>
2025-10-18 15:10:54 -0400

    Update Car.cpp

--- src/Car.cpp
+++ src/Car.cpp
@@ -1,5 +1,9 @@
 #include "Car.hpp"
+#include <algorithm>
 
 Car::Car() {}
 
 // Write implemenation for your energy model here
+double Car::total_energy_loss() {
+	return aero_loss() + rolling_loss() + gravitational_loss() + passive_energy_loss();
+}
